0fa8c13b7e224064eec96535610314b78dedd641
shrutipandey005 <pandeyshruti126@gmail.com>
2026-07-07 13:11:21 +0530

    Added prime program

---
+++ PrimeNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PrimeRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+
+        for (int i = start; i <= end; i++) {
+            int count = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}

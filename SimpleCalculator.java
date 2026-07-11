17b12d8b61e54ea995a268c7b13aa4f4b647d6b2
shrutipandey005 <pandeyshruti126@gmail.com>
2026-07-11 20:20:46 +0530

    Added SimpleCalculator

---
+++ SimpleCalculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        char op = sc.next().charAt(0);
+
+        if (op == '+') {
+            System.out.println(a + b);
+        } else if (op == '-') {
+            System.out.println(a - b);
+        } else if (op == '*') {
+            System.out.println(a * b);
+        } else if (op == '/') {
+            System.out.println(a / b);
+        } else {
+            System.out.println("Invalid operator");
+        }
+    }
+}

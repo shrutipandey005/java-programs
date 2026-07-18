ae9438e14e24672f53515d249812d26ce81161a1
shrutipandey005 <pandeyshruti126@gmail.com>
2026-07-18 23:03:16 +0530

    Added student class program

---
+++ Student.java
@@ -0,0 +1,16 @@
+class Student {
+    int id;
+    String name;
+
+    void display() {
+        System.out.println(id + " " + name);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.id = 101;
+        s1.name = "John";
+
+        s1.display();
+    }
+}
